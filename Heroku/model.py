bad2efef81552cba86ae2c1f52dec5a26d583a3a
Putul Singh <127419636+putul03@users.noreply.github.com>
2023-10-26 18:37:04 +0530

    Add files via upload

---
+++ Heroku/model.py
@@ -0,0 +1,14 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import pickle
+dataset = pd.read_csv('Salary_Data.csv')
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, -1].values
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 1/3, random_state = 0)
+from sklearn.linear_model import LinearRegression
+regressor = LinearRegression()
+regressor.fit(X_train, y_train)
+y_pred = regressor.predict(X_test)
+pickle.dump(regressor, open('model.pkl','wb'))
